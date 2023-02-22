69220f43131f89d652536e62be47f47e99b6c862
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-02-22 06:25:31 +0100

    Desktop design done, responsive design to do

--- src/global-scripts/scripts/counter/counter.js
+++ src/global-scripts/scripts/counter/counter.js
@@ -3,11 +3,11 @@
 /// so that they can be used in other files
 
 export function setupCounter(element) {
-  let counter = 0
-  const setCounter = (count) => {
-    counter = count
-    element.innerHTML = `script test - count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+//   let counter = 0
+//   const setCounter = (count) => {
+//     counter = count
+//     element.innerHTML = `script test - count is ${counter}`
+//   }
+//   element.addEventListener('click', () => setCounter(counter + 1))
+//   setCounter(0)
 }
