941e1c018433dd592c43939395868a6d7818f886
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-03-10 15:17:25 +0100

    Desktop and Tablet versions fixed, Mobile to do

--- src/pages/home-page/scripts/home-page.js
+++ src/pages/home-page/scripts/home-page.js
@@ -2,6 +2,9 @@
 /// Here we import all the JavaScript files we need for our homepage.
 
 import '../styles/home-page.scss'
+import '../styles/header.scss'
+import '../styles/ai-section.scss'
+import '../styles/ai-box.scss'
 
 import { setupCounter } from '../../../global-scripts/scripts/counter/counter.js'
 
