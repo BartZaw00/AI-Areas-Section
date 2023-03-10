92b2b85f6f2698cef170fe2d1bd62055d18826ea
Bartosz Zawadka <bart.zaw00@gmail.com>
2023-03-10 16:08:59 +0100

    Project done

--- src/pages/home-page/scripts/home-page.js
+++ src/pages/home-page/scripts/home-page.js
@@ -1,7 +1,7 @@
 /// Homepage JavaScript File
 /// Here we import all the JavaScript files we need for our homepage.
 
-import '../styles/home-page.scss'
+import '../styles/home.scss'
 import '../styles/header.scss'
 import '../styles/ai-section.scss'
 import '../styles/ai-box.scss'
